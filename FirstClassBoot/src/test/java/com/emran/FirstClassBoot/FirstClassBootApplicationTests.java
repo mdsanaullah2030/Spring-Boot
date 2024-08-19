669e76dd4bf6ee7a0914798ce42e3f3578835247
MD. SANAULLAH <158471899+mdsanaullah2030@users.noreply.github.com>
2024-08-19 23:15:40 +0600

    add

---
+++ FirstClassBoot/src/test/java/com/emran/FirstClassBoot/FirstClassBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emran.FirstClassBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstClassBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
