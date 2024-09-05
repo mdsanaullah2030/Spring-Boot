cc23901ae8a69877dc45a5012315ec1a845fd225
mdsanaullah2030 <sanaullah1997215@gmail.com>
2024-09-05 15:58:53 +0600

    add

--- HotelBooking/src/main/java/com/booking/HotelBooking/restcontroller/HotolController.java
+++ HotelBooking/src/main/java/com/booking/HotelBooking/restcontroller/HotolController.java
@@ -1,9 +1,35 @@
 package com.booking.HotelBooking.restcontroller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.booking.HotelBooking.entity.Hotel;
+import com.booking.HotelBooking.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("")
 public class HotolController {
+
+
+    @Autowired
+    private HotelService hotelService;
+@PostMapping("/save")
+    public ResponseEntity<String>saveHtel(
+        @RequestPart(value ="hotel" ) Hotel hotel,
+        @RequestParam(value = "image",required = true)MultipartFile file
+        ) throws IOException {
+    hotelService.saveHotel(hotel,file);
+return new ResponseEntity<>("Hotel Added successful", HttpStatus.OK);
+    }
+
+
+
+
+
+
 }
--- HotelBooking/src/main/java/com/booking/HotelBooking/service/HotelService.java
+++ HotelBooking/src/main/java/com/booking/HotelBooking/service/HotelService.java
@@ -28,50 +28,47 @@ public class HotelService {
     @Value("src/main/resources/static/images")
     private String uploadDir;
 
-    public List<Hotel>getAllHotels(){
+    public List<Hotel> getAllHotels() {
         return hotelRepositoy.findAll();
     }
 
-    public void saveHotel(Hotel hotel,MultipartFile imageFile) throws IOException {
-        Location location=locationRepository.findById(hotel.getLocation().getId())
-                .orElseThrow(()->new RuntimeException("Location not found") );
+    public void saveHotel(Hotel hotel, MultipartFile imageFile) throws IOException {
+        Location location = locationRepository.findById(hotel.getLocation().getId())
+                .orElseThrow(() -> new RuntimeException("Location not found"));
 
 
-
-        if (imageFile !=null && !imageFile.isEmpty()){
-            String imageFilename=saveImage(imageFile,hotel);
+        if (imageFile != null && !imageFile.isEmpty()) {
+            String imageFilename = saveImage(imageFile, hotel);
             hotel.setImage(imageFilename);
         }
 
-             hotel.setLocation(location);
-             hotelRepositoy.save(hotel);
+        hotel.setLocation(location);
+        hotelRepositoy.save(hotel);
 
     }
 
-    public void deleteHotelBuId(int id ){
+    public void deleteHotelBuId(int id) {
         hotelRepositoy.deleteById(id);
     }
 
-    public Hotel findById(int id){
-        return hotelRepositoy.findById(id).orElseThrow(()->new RuntimeException("Hotel not Fond"));
+    public Hotel findById(int id) {
+        return hotelRepositoy.findById(id).orElseThrow(() -> new RuntimeException("Hotel not Fond"));
 
     }
 
-    private String saveImage(MultipartFile file ,Hotel h) throws IOException{
-        Path uploadPath= Paths.get(uploadDir+"/hotel");
-        if (!Files.exists(uploadPath)){
+    private String saveImage(MultipartFile file, Hotel h) throws IOException {
+        Path uploadPath = Paths.get(uploadDir + "/hotel");
+        if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
         }
-            String filename= h.getName()+"_"+UUID.randomUUID().toString();
-            Path filePath=uploadPath.resolve(filename);
+        String filename = h.getName() + "_" + UUID.randomUUID().toString();
+        Path filePath = uploadPath.resolve(filename);
 
-            Files.copy(file.getInputStream(),filePath);
-            return filename;
+        Files.copy(file.getInputStream(), filePath);
+        return filename;
 
     }
-
-
-
-
-
 }
+
+
+
